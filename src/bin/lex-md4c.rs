954045cb9bbfa7dcbf7689abbdee933912cd49ac
ad-chaos <santoshrajpurohit89@gmail.com>
2025-02-05 22:41:20 +0530

    parse once

--- src/bin/lex-md4c.rs
+++ src/bin/lex-md4c.rs
@@ -4,17 +4,11 @@ use klang::Lexer;
 
 type AppResult = Result<(), Box<dyn Error>>;
 
-fn inflates_to(size: usize, repeats: usize) {
-    let size = size << repeats;
-    println!("File size: {:.2} MB", (size as f64 / (1024. * 1024.)));
-}
-
 fn main() -> AppResult {
     let filename = std::env::args().nth(1).ok_or("Not enough arguments")?;
     let mut file = std::fs::File::open(filename)?;
-    let mut source = Vec::new();
+    let mut source = Vec::with_capacity(60 * 1024 * 1024);
     file.read_to_end(&mut source)?;
-    let orig_size = source.len();
 
     let last = *source.last().expect("zero length file");
 
@@ -27,34 +21,19 @@ fn main() -> AppResult {
         .collect();
     source.push(last);
 
-    const N: usize = 5;
-    for _ in 0..N {
-        source.extend_from_within(..);
-    }
-    inflates_to(orig_size, N);
-
     let start = Instant::now();
-
-    for _ in 0..5 {
-        let start = Instant::now();
-        let mut lex = Lexer::new(&source);
-        let mut ntokens = 0;
-        loop {
-            let token = lex.next_token()?;
-            if token.is_eof() {
-                break;
-            }
-            ntokens += 1;
+    let mut lex = Lexer::new(&source);
+    let mut ntokens = 0;
+    loop {
+        let token = lex.next_token()?;
+        if token.is_eof() {
+            break;
         }
-
-        println!(
-            "{} tokens, {:.2} tokens/μs",
-            ntokens,
-            ntokens as f64 / start.elapsed().as_micros() as f64,
-        );
+        ntokens += 1;
     }
+    let end = start.elapsed().as_micros();
 
-    println!("Total time: {}ms", start.elapsed().as_millis());
+    println!("{} tokens/μs, {:.2}ms", ntokens / end, end as f64 / 1000.);
 
     Ok(())
 }
